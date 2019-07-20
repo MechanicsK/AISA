f4ddfa1f4e2c486120aecb0fc23e734b6d7cd4bd
Vikas-Rick <53000494+Vikas-Rick@users.noreply.github.com>
2019-07-20 12:54:42 +0530

    Update views.py
    add views.py in aisa

--- aisa/views.py
+++ aisa/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+#Create your views here.
+def index(request):
+   return HttpResponse("hello,")
+def index(request):
+    return render(request,'header.html')
